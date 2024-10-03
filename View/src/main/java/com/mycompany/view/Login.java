cd1e9b83c65e58edf315e9133fa7d8d06d570b14
Jonatan997 <126485221+Jonatan997@users.noreply.github.com>
2024-10-03 18:32:01 -0300

    Primera Version

---
+++ View/src/main/java/com/mycompany/view/Login.java
@@ -0,0 +1,212 @@
+package com.mycompany.view;
+
+import BusinessObjects.UsuarioLocal;
+import BusinessObjects.Usuarios;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Login extends javax.swing.JFrame {
+
+    // Obtener la instancia única de UsuarioLocal
+    UsuarioLocal usuario = UsuarioLocal.getInstancia();
+
+    ArrayList<Usuarios> listaUsuarios;
+
+    Usuarios users = new Usuarios();
+
+    public Login() {
+        initComponents();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jInternalFrame1 = new javax.swing.JInternalFrame();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        nombreUsuario = new javax.swing.JTextField();
+        contrasenia = new javax.swing.JPasswordField();
+        jLabel1 = new javax.swing.JLabel();
+
+        jInternalFrame1.setVisible(true);
+
+        javax.swing.GroupLayout jInternalFrame1Layout = new javax.swing.GroupLayout(jInternalFrame1.getContentPane());
+        jInternalFrame1.getContentPane().setLayout(jInternalFrame1Layout);
+        jInternalFrame1Layout.setHorizontalGroup(
+            jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        jInternalFrame1Layout.setVerticalGroup(
+            jInternalFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Copperplate Gothic Bold", 2, 12)); // NOI18N
+        jLabel2.setText("CONTRASEÑA:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 370, 270, 20));
+
+        jLabel3.setFont(new java.awt.Font("Edwardian Script ITC", 1, 70)); // NOI18N
+        jLabel3.setText("F");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 40, 90, 100));
+
+        jLabel5.setFont(new java.awt.Font("Copperplate Gothic Bold", 2, 36)); // NOI18N
+        jLabel5.setText("BOOKFLOW");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 190, 270, 100));
+
+        jLabel4.setFont(new java.awt.Font("Copperplate Gothic Bold", 2, 12)); // NOI18N
+        jLabel4.setText("USUARIO:");
+        jLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 290, 270, 20));
+
+        jLabel6.setFont(new java.awt.Font("Edwardian Script ITC", 1, 70)); // NOI18N
+        jLabel6.setText("B");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 90, 100));
+
+        jButton1.setBackground(new java.awt.Color(204, 255, 204));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton1.setText("ACEPTAR");
+        jButton1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 460, 110, 40));
+
+        nombreUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nombreUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(nombreUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 310, 190, 40));
+        jPanel1.add(contrasenia, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 390, 190, 40));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo.png"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 990, 660));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void nombreUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nombreUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nombreUsuarioActionPerformed
+
+    //BOTON ACEPTAR
+    //COMPRUEBA QUE EL USUARIO Y LA CONTRASEÑAS IGRESADOS SEAN VALIDOS
+    //AUTOR: JONATAN ZAMORA
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        //obtenemos el usuario y la contraseña ingresada
+        String nombre = nombreUsuario.getText();
+        String contraseña = contrasenia.getText();
+
+        //obtenemos la lista de usuarios para chequear
+        listaUsuarios = users.getListaUsuarios();
+
+        if (nombre.equals("admin") && contraseña.equals("admin")) {
+
+            //la guardamos en el objeto UsuarioLocal
+            usuario.setUsuario(nombre);
+            usuario.setContraseña(contraseña);
+            usuario.setNivel(1);
+
+            MenuPrincipal menu = new MenuPrincipal();
+            menu.setVisible(true);
+
+        }
+
+        //comprobamos que los usuarios ingresados se encuentren en la base de datos
+        for (Usuarios usuarios : listaUsuarios) {
+
+            if (usuarios.getNombre().equals(nombre) && usuarios.getContraseña().equals(contraseña)) {
+
+                //la guardamos en el objeto UsuarioLocal
+                usuario.setUsuario(nombre);
+                usuario.setContraseña(contraseña);
+                usuario.setNivel(usuarios.getNivel());
+
+                MenuPrincipal menu = new MenuPrincipal();
+                menu.setVisible(true);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario y/o contraseña incorrectos", "ERROR EN INICIO DE SECIÓN", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        }
+
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPasswordField contrasenia;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JInternalFrame jInternalFrame1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField nombreUsuario;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ View/src/main/java/com/mycompany/view/View.java
@@ -0,0 +1,21 @@
+package com.mycompany.view;
+
+import ModelException.ModelException;
+
+
+/**
+ *
+ * @author 54234
+ */
+public class View {
+
+    public static void main(String[] args) throws ModelException {
+        System.out.println("SOFTWARE INICIADO");
+        
+        Login login = new Login();
+        login.setVisible(true);
+
+
+        
+    }
+}
