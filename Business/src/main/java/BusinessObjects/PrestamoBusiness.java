310b5401c637d32920f4d4d047225f719fa5db67
Jonatan997 <126485221+Jonatan997@users.noreply.github.com>
2024-10-04 23:36:32 -0300

    Merge origin/master
    # Conflicts:
    #	Business/src/main/java/BusinessObjects/SociosBusiness.java

---
+++ Business/src/main/java/BusinessObjects/PrestamoBusiness.java
@@ -0,0 +1,15 @@
+
+package BusinessObjects;
+
+import java.util.Date;
+
+
+public class PrestamoBusiness {
+    
+    public boolean prestar(int idSocio, long numID, Date fechaRetorno){
+        
+        
+    }
+    
+    
+}
