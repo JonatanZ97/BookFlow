769a1e536d6a2edf206c720c3eb2365117492388
david <david@iQual_NQ1>
2024-10-07 20:56:45 -0300

    Merge origin/master
    # Conflicts:
    #	View/src/main/java/com/mycompany/view/ModificacionSocio.java
    #	View/src/main/java/com/mycompany/view/ModificarLibro.java

--- View/src/main/java/com/mycompany/view/ListaReservas.java
+++ View/src/main/java/com/mycompany/view/ListaReservas.java
@@ -4,7 +4,15 @@
  */
 package com.mycompany.view;
 
+import BusinessObjects.LibrosBusiness;
+import BusinessObjects.PrestamoBusiness;
 import BusinessObjects.ReservaBusiness;
+import BusinessObjects.SociosBusiness;
+import ModelException.ModelException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -35,13 +43,13 @@ public class ListaReservas extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         botonMostrar = new javax.swing.JButton();
-        campDato = new javax.swing.JTextField();
+        campDatos = new javax.swing.JTextField();
         jLabel14 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        campDatos = new javax.swing.JTextArea();
+        areaTexto = new javax.swing.JTextArea();
         jLabel15 = new javax.swing.JLabel();
         botonId = new javax.swing.JRadioButton();
-        BotonDni = new javax.swing.JRadioButton();
+        botonDni = new javax.swing.JRadioButton();
         botonIdentificador = new javax.swing.JRadioButton();
         botonBuscar = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
@@ -91,20 +99,20 @@ public class ListaReservas extends javax.swing.JFrame {
         });
         jPanel1.add(botonMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 390, 140, 40));
 
-        campDato.addActionListener(new java.awt.event.ActionListener() {
+        campDatos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                campDatoActionPerformed(evt);
+                campDatosActionPerformed(evt);
             }
         });
-        jPanel1.add(campDato, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 250, 30));
+        jPanel1.add(campDatos, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 250, 30));
 
         jLabel14.setFont(new java.awt.Font("Copperplate Gothic Bold", 0, 12)); // NOI18N
         jLabel14.setText("RESULTADO DE BUSQUEDA:");
         jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 20, 240, 30));
 
-        campDatos.setColumns(20);
-        campDatos.setRows(5);
-        jScrollPane1.setViewportView(campDatos);
+        areaTexto.setColumns(20);
+        areaTexto.setRows(5);
+        jScrollPane1.setViewportView(areaTexto);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 50, 610, 560));
 
@@ -116,9 +124,14 @@ public class ListaReservas extends javax.swing.JFrame {
         botonId.setText("ID SOCIO");
         jPanel1.add(botonId, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 140, -1, -1));
 
-        buttonGroup1.add(BotonDni);
-        BotonDni.setText("DNI");
-        jPanel1.add(BotonDni, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, -1, -1));
+        buttonGroup1.add(botonDni);
+        botonDni.setText("DNI");
+        botonDni.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonDniActionPerformed(evt);
+            }
+        });
+        jPanel1.add(botonDni, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, -1, -1));
 
         buttonGroup1.add(botonIdentificador);
         botonIdentificador.setText("NUMERO DE IDENTIFICACIÓN DEL LIBRO");
@@ -190,16 +203,114 @@ public class ListaReservas extends javax.swing.JFrame {
 
     private void botonMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonMostrarActionPerformed
         ReservaBusiness reserva = new ReservaBusiness();
-        campDatos.setText("");
+        areaTexto.setText("");
         String texto;
-        
+        texto = areaTexto.getText();
+        ArrayList<String> listaString = reserva.obtenerListaReservas();
+
+        StringBuilder contenido = new StringBuilder();
+        for (String elemento : listaString) {
+            contenido.append(elemento).append("\n");  // Cada elemento en una nueva línea
+        }
+
+        // Mostrar el contenido en el TextArea
+        areaTexto.setText(contenido.toString());
+
     }//GEN-LAST:event_botonMostrarActionPerformed
 
-    private void campDatoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campDatoActionPerformed
+    private void campDatosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campDatosActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_campDatoActionPerformed
+    }//GEN-LAST:event_campDatosActionPerformed
 
     private void botonBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonBuscarActionPerformed
+        String texto;
+        SociosBusiness socio = new SociosBusiness();
+        LibrosBusiness libro = new LibrosBusiness();
+        if (botonId.isSelected()) {//boton id
+            try {
+                //obtengo los numeros de los campos de textos
+                texto = campDatos.getText();
+
+                // Convierto el texto a int
+                int id = Integer.parseInt(texto);
+
+                boolean respuestaS = socio.existeSocioID(id);
+
+                if (respuestaS) {
+                    areaTexto.setText("");
+                    ReservaBusiness reserva = new ReservaBusiness();
+                    ArrayList<String> listaString = reserva.buscarPorIdSocio(id);
+                    StringBuilder contenido = new StringBuilder();
+                    for (String elemento : listaString) {
+                        contenido.append(elemento).append("\n");  // Cada elemento en una nueva línea
+                    }
+
+                    // Mostrar el contenido en el TextArea
+                    areaTexto.setText(contenido.toString());
+                } else if (!respuestaS) {
+                    JOptionPane.showMessageDialog(null, "No existe un socio con ese id");
+                }
+            } catch (NumberFormatException e) {
+                // Manejar el error si el texto no es un número válido
+                JOptionPane.showMessageDialog(null, "Ingrese un numero valido");
+            }
+        } else if (botonDni.isSelected()) {
+            try {
+                //obtengo los numeros de los campos de textos
+                texto = campDatos.getText();
+
+                // Convierto el texto a long
+                Long dni = Long.parseLong(texto);
+
+                boolean respuestaS = socio.existeSocioDNI(dni);
+
+                if (respuestaS) {
+                    areaTexto.setText("");
+                    ReservaBusiness reserva = new ReservaBusiness();
+                    ArrayList<String> listaString = reserva.buscarPorDNI(dni);
+                    StringBuilder contenido = new StringBuilder();
+                    for (String elemento : listaString) {
+                        contenido.append(elemento).append("\n");  // Cada elemento en una nueva línea
+                    }
+
+                    // Mostrar el contenido en el TextArea
+                    areaTexto.setText(contenido.toString());
+                } else if (!respuestaS) {
+                    JOptionPane.showMessageDialog(null, "No existe un socio con ese dni");
+                }
+            } catch (NumberFormatException e) {
+                // Manejar el error si el texto no es un número válido
+                JOptionPane.showMessageDialog(null, "Ingrese un numero valido");
+            }
+        } else if (botonIdentificador.isSelected()) {
+            try {
+                //obtengo los numeros de los campos de textos
+                texto = campDatos.getText();
+
+                // Convierto el texto a long
+                Long identificacion = Long.parseLong(texto);
+
+                boolean respuestaL = libro.existeLibro(identificacion);
+
+                if (respuestaL) {
+                    areaTexto.setText("");
+                    ReservaBusiness reserva = new ReservaBusiness();
+                    ArrayList<String> listaString = reserva.buscarPorNumeroIdentificacion(identificacion);
+                    StringBuilder contenido = new StringBuilder();
+                    for (String elemento : listaString) {
+                        contenido.append(elemento).append("\n");  // Cada elemento en una nueva línea
+                    }
+
+                    // Mostrar el contenido en el TextArea
+                    areaTexto.setText(contenido.toString());
+                } else if (!respuestaL) {
+                    JOptionPane.showMessageDialog(null, "No existe un libro con ese numero de identificacion");
+                }
+            } catch (NumberFormatException e) {
+                // Manejar el error si el texto no es un número válido
+                JOptionPane.showMessageDialog(null, "Ingrese un numero valido");
+            }
+        }
         // TODO add your handling code here:
     }//GEN-LAST:event_botonBuscarActionPerformed
 
@@ -209,6 +320,10 @@ public class ListaReservas extends javax.swing.JFrame {
         this.dispose();
         // TODO add your handling code here:
     }//GEN-LAST:event_jToggleButton3ActionPerformed
+
+    private void botonDniActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonDniActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botonDniActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,7 +351,7 @@ public class ListaReservas extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(ListaReservas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        
+
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -246,14 +361,14 @@ public class ListaReservas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JRadioButton BotonDni;
+    private javax.swing.JTextArea areaTexto;
     private javax.swing.JButton botonBuscar;
+    private javax.swing.JRadioButton botonDni;
     private javax.swing.JRadioButton botonId;
     private javax.swing.JRadioButton botonIdentificador;
     private javax.swing.JButton botonMostrar;
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JTextField campDato;
-    private javax.swing.JTextArea campDatos;
+    private javax.swing.JTextField campDatos;
     private javax.swing.JLabel fondo;
     private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel14;
